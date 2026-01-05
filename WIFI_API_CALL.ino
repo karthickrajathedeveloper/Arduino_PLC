4686eac6401c5f261ebb88429f0f6aeb9565a07f
Karthickraja Murugan <130274513+karthickrajathedeveloper@users.noreply.github.com>
2026-01-05 12:51:50 +0530

    PLC call post API

---
+++ WIFI_API_CALL.ino
@@ -0,0 +1,49 @@
+#include <WiFi.h>
+#include <ArduinoHttpClient.h>
+
+const char* ssid = "Petal";
+const char* password = "Petal#2024";
+
+const char* serverIP = "192.168.1.12"; // Flask PC IP
+int port = 8080;
+
+WiFiClient wifi;
+HttpClient client(wifi, serverIP, port);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected");
+
+  sendAPICall();   
+}
+
+void loop() {}
+
+void sendAPICall() {
+  Serial.println("Sending API call...");
+
+  client.beginRequest();
+  client.post("/plc/button");
+  client.sendHeader("Content-Type", "application/json");
+
+  String body = "{\"button\":\"JEEVA\"}";
+  client.sendHeader("Content-Length", body.length());
+
+  client.beginBody();
+  client.print(body);
+  client.endRequest();
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status Code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+}
